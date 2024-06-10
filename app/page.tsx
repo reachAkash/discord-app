1f4ed4889a1e1d130ac0eadb5f7d08f37bebce10
reachAkash <akashgupta9062@gmail.com>
2024-06-10 22:28:15 +0530

    update : organisational folders

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
+  const state = true;
   return (
+    <div className="">
     <p className="text-red-500">Discord App</p>
+    <Button className={cn(
+       "bg-yellow-500", state && "bg-green-500",
+    )}>Akash</Button>
+    </div>
   );
 }
---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Test Page</div>
+  )
+}
+
+export default page
