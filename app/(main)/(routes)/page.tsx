5c6519a2d76a970f5d66e2174e502ed9bb11a9cc
reachAkash <akashgupta9062@gmail.com>
2024-06-12 21:38:01 +0530

    update : dark mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   const state = true;
   return (
     <div className="">
-      This is a protected route.
+      <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
